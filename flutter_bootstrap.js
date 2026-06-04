2f65068ac1a8a63db9ef1b1690e138c4382bfb5a
Praveenkrishna-ait <Praveenkrishna-ait@users.noreply.github.com>
2026-06-04 05:23:24 +0000

    deploy: ce8ce75f980214a2fd63df571b75e1b83cd32359

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "944580260" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1491346872" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
